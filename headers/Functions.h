3518e4b5cbf641238d3a337a555d2bd8ec3b0480
zimon-cocos <111566187+zimon-cocos@users.noreply.github.com>
2025-04-18 12:20:11 +0200

    Edge cases WIP

--- headers/Functions.h
+++ headers/Functions.h
@@ -8,43 +8,58 @@ void checkCells(int cellNum)
 
 
     //sused S
-    if(cellNum - boxesX >=0)
+    if(cellNum - boxesX >0)
     {
-        std::cout << "Checking " << cellNum-boxesX << '\n';
+        std::cout << "Checking S " << cellNum-boxesX << '\n';
         boxes[cellNum-boxesX].shape.setFillColor(sf::Color::Green);
     }
 
     //sused J
-
     boxes[cellNum+boxesX].shape.setFillColor(sf::Color::Green);
-    //sused V
-    boxes[cellNum+1].shape.setFillColor(sf::Color::Green);
-    //sused Z
+    std::cout << "Checking J " << cellNum+boxesX<< '\n';
 
-    if(cellNum-1>=0)
+    //sused V
+    if(boxes[cellNum+1].gridYValue == boxes[cellNum].gridYValue)
+    {
+        boxes[cellNum+1].shape.setFillColor(sf::Color::Green);
+    }
+
+    std::cout << "Checking V " << cellNum+1<< '\n';
+
+    //sused Z
+    if(cellNum-1>0 && (boxes[cellNum-1].gridYValue == boxes[cellNum].gridYValue))
     {
         boxes[cellNum-1].shape.setFillColor(sf::Color::Green);
+        std::cout << "Checking Z " <<cellNum-1 << '\n';
     }
 
     //sused SZ
-    if(cellNum-boxesX+1>=0)
+    if(cellNum-boxesX+1>0)
     {
         boxes[cellNum-boxesX+1].shape.setFillColor(sf::Color::Green);
+        std::cout << "Checking SZ " <<cellNum-boxesX+1 << '\n';
     }
 
     //sused SV
-    if(cellNum-boxesX+1>=0)
+    if(cellNum-boxesX+1>0)
     {
         boxes[cellNum-boxesX-1].shape.setFillColor(sf::Color::Green);
+        std::cout << "Checking SV " << cellNum-boxesX+1 << '\n';
     }
 
     //sused JZ
-    boxes[cellNum+boxesX+1].shape.setFillColor(sf::Color::Green);
-    //sused JV
+    if((boxes[cellNum+boxesX+1].gridYValue-1) == boxes[cellNum].gridYValue)
+    {
+        boxes[cellNum+boxesX+1].shape.setFillColor(sf::Color::Green);
+    }
 
-    if(cellNum+boxesX-1>=0)
+    std::cout << "Checking JZ " << cellNum+boxesX+1<< '\n';
+
+    //sused JV
+    if(cellNum+boxesX-1>0 && boxes[cellNum+boxesX-1].gridYValue - 1 == boxes[cellNum].gridYValue)
     {
         boxes[cellNum+boxesX-1].shape.setFillColor(sf::Color::Green);
+        std::cout << "Checking JV " <<cellNum+boxesX << '\n';
     }
 
 
